899138f4dc38edf695d2b184cb7edbe53f6f0463
AbhishekAdvaniExpound <abhishek.advani@expoundtechnivo.com>
2025-09-25 12:46:56 +0530

    changes

--- frontend/src/api/authApi.js
+++ frontend/src/api/authApi.js
@@ -1,3 +1,3 @@
 // https://pwaapppoc.onrender.com
-// export const API_BASE = "https://pwaapppoc.onrender.com";
-export const API_BASE = "http://localhost:5000";
+export const API_BASE = "https://pwaapppoc.onrender.com";
+// export const API_BASE = "http://localhost:5000";
